0b4f54ff25846fa4098b61d14779d5093219440c
KirillVM <forjustice55@gmail.com>
2024-01-22 00:42:22 +0300

    init: add start react project configuration

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <header>
+        <h1>
+          Hello world!
+        </h1>
+      </header>
+    </>
+  )
+}
+
+export default App;
